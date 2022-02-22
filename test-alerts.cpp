c44fcd43f947b708b2e38fde3de9d4291ca4ab9d
ChaithraBallal <91113671+ChaithraBallal@users.noreply.github.com>
2022-02-23 00:31:23 +0530

    Update test-alerts.cpp

--- test-alerts.cpp
+++ test-alerts.cpp
@@ -14,6 +14,14 @@ TEST_CASE("infers the breach according to cooling types")
 {
   
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, -4) == TOO_LOW);
-  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 89) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 36) == TOO_HIGH);
   REQUIRE(classifyTemperatureBreach(PASSIVE_COOLING, 26) == NORMAL);
+  
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, -6) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 90) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(HI_ACTIVE_COOLING, 40) == NORMAL);
+  
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, -45) == TOO_LOW);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 42) == TOO_HIGH);
+  REQUIRE(classifyTemperatureBreach(MED_ACTIVE_COOLING, 39) == NORMAL);
 }
